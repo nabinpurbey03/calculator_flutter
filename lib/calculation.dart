a63e34fa27a3995c84c1a421b0352b30149690a4
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-07-17 00:46:44 +0545

    inital commit

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,26 +1,42 @@
 import 'dart:io';
 
-String calculation(String data) {
+String calculation(String expn) {
+  String op1 = "";
+  String op2 = "";
+  String opr = "";
+  String opr2 = "";
   String result = "";
-  String operand1 = "";
-  String operand2 = "";
-  String operator = "";
-  // setState(() {});
-  for (int i = 0; i < data.length; i++) {
-    String sc = data[i];
-    if (sc == "+" || sc == "-" || sc == "*" || sc == "/" || sc == "%") {
-      operator = data[i];
+  String checkExpn = "";
+  for (int i = 0; i < expn.length; i++) {
+    String dig = expn[i];
+    checkExpn = expn.substring(i + 1, expn.length);
+    if (dig == "+" || dig == "-" || dig == "*" || dig == "/" || dig == "%") {
+      opr = dig;
       break;
-    } else {
-      operand1 += data[i];
     }
+    op1 += expn[i];
   }
-  int operatorIndex = data.indexOf(operator);
-  for (int i = operatorIndex + 1; i < data.length; i++) {
-    operand2 += data[i];
+  int opi = expn.indexOf(opr);
+  for (int i = opi + 1; i < expn.length; i++) {
+    checkExpn = expn.substring(i + 1, expn.length);
+    String dig = expn[i];
+    if (dig == "+" || dig == "-" || dig == "*" || dig == "/" || dig == "%") {
+      opr2 = dig;
+      break;
+    }
+    op2 += expn[i];
   }
-  var num1 = double.tryParse(operand1);
-  var num2 = double.tryParse(operand2);
+  result = calculator(op1, op2, opr);
+  if (checkExpn.isNotEmpty) {
+    result = calculation(result + opr2 + checkExpn);
+  }
+  return result;
+}
+
+String calculator(String operand1, String operand2, String operator) {
+  var num1 = int.tryParse(operand1);
+  var num2 = int.tryParse(operand2);
+  String result = "";
   if (num1 != null && num2 != null) {
     switch (operator) {
       case "+":
@@ -33,10 +49,10 @@ String calculation(String data) {
         result = (num1 * num2).toString();
         break;
       case "/":
-        result = (num1 / num2).toString();
+        result = (num1 / num2).ceil().toString();
         break;
       case "%":
-        result = (num1 * (num2 / 100)).toString();
+        result = (num1 * (num2 / 100)).ceil().toString();
         break;
     }
   }
